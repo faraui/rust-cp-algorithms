72d7c9421a0181042570b5fb40ac88e67569b2da
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-15 13:23:45 +0200

    ++ not-sec-hash.rs

--- src/not-sec-hash.rs
+++ src/not-sec-hash.rs
@@ -5,8 +5,7 @@ fn get(p: &Vec<usize>, h: &Vec<usize>, l: usize, r: usize) -> usize {
     (h[r + 1] - (h[l] * p[r - l + 1]) % M + M) % M
 }
 
-fn main() {
-    let s = String::from("Eum at quia et. Iusto et numquam ab incidunt enim.");
+fn hash(s: &str) -> usize {
     let n = s.len();
     let mut p = vec![1; n + 1];
     let mut h = vec![0; n + 1];
@@ -14,5 +13,5 @@ fn main() {
         p[i] = (p[i - 1] * B) % M;
         h[i] = (h[i - 1] * B + (s.as_bytes()[i - 1] - b'a' + 1) as usize) % M;
     }
-    println!("{}", get(&p, &h, 0, 2),)
+    get(&p, &h, 0, 2)
 }
